6ceaa962f229314c3900af5d0427061170fb7f3b
Gmarin10 <gkm239@nyu.edu>
2019-07-25 16:13:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-import matchFunctions
+import BastaMatchingFunctions
 import airtable
-import datetime
 
-#get tables from base
-fellows = matchFunctions.getTable('Fellows')
-jobs = matchFunctions.getTable('Jobs')
-matches = matchFunctions.getTable('Matches')
+#Basta airtable API Keys
+AIRTABLE_API_KEY = 'keyi9WRicMVH9g2ui'
+##BASTA_BASE = 'app3yyj1S4SMRTGw6'
 
-matchFunctions.cycleTables(jobs,fellows,matches)
+#Test base key
+BASTA_BASE = 'appC07SJAr7flkFfO'
+
+#Get required tables from Basta Base
+fellows = BastaMatchingFunctions.getTable(BASTA_BASE, 'Fellows', AIRTABLE_API_KEY)
+jobs = BastaMatchingFunctions.getTable(BASTA_BASE, 'Jobs', AIRTABLE_API_KEY)
+matches = BastaMatchingFunctions.getTable(BASTA_BASE, 'Matches', AIRTABLE_API_KEY)
+
+BastaMatchingFunctions.MatchOpps(jobs,fellows,matches)
